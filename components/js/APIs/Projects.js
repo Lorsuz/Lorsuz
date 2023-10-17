b532c8f81bf2038b6aa60ff6e0e02536086b4287
Lorsuz <91427731+Lorsuz@users.noreply.github.com>
2023-10-17 14:49:07 -0300

    dsfsdfs

--- components/js/APIs/Projects.js
+++ components/js/APIs/Projects.js
@@ -8,20 +8,22 @@ fetch( "data.json" ).then( ( response ) => {
 				<li class="card">
 					<div class="img"><img src="components/static/img/projects/${card.image}" alt="${card.image}">
 					</div>
-					<div class="details">
-					<h3>${card.project}</h3>
-						<div class="skill-tags">${
-							card.skills.map( ( skill ) => {
-								return `<span class="tag">${skill}</span>`;
-							} ).join( "" )
-						}
-						</div>
-						<p>${card.description}</p>
-						<div class="links">
-							<a href="https://github.com/Lorsuz/${card.repository}" target="_blank">Repository</a>
-							${
-								card.pages ?	`<a href="${card.pages}" target="_blank">Web Site</a>`:""
+					<div class="animation">
+						<div class="details">
+						<h3>${card.project}</h3>
+							<div class="skill-tags">${
+								card.skills.map( ( skill ) => {
+									return `<span class="tag">${skill}</span>`;
+								} ).join( "" )
 							}
+							</div>
+							<p>${card.description}</p>
+							<div class="links">
+								<a href="https://github.com/Lorsuz/${card.repository}" target="_blank">Repository</a>
+								${
+									card.pages ?	`<a href="${card.pages}" target="_blank">Web Site</a>`:""
+								}
+							</div>
 						</div>
 					</div>
 				</li>
